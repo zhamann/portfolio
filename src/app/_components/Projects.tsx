80be7092d6a84fc9c0a77cb99f407c43a052a76c
Zac Hamann <hamannzac@gmail.com>
2024-10-05 08:55:09 -0500

    adjust projects page

--- src/app/_components/Projects.tsx
+++ src/app/_components/Projects.tsx
@@ -2,27 +2,31 @@ import Image from "next/image";
 
 export default function Projects() {
   return (
-    <div className="flex shrink-0 items-center gap-4 overflow-x-auto pb-4 md:gap-8">
+    <div className="flex shrink-0 items-stretch gap-4 overflow-x-auto py-6 pe-8 ps-[50vw] md:gap-8 md:pe-16 md:ps-16">
       {projects.map((project, index) => (
         <a
           key={index}
           href={project.href}
-          className="flex flex-col gap-4 rounded-lg bg-slate-900 px-4 py-8"
+          className="flex -translate-x-1/2 flex-col rounded-lg border border-slate-800 bg-slate-900 transition-transform duration-300 ease-in-out hover:z-10 hover:scale-105 hover:border-indigo-500 md:translate-x-0"
         >
-          <div className="flex items-center justify-center px-12">
-            <div className="relative h-32 w-32">
+          <div className="flex items-center justify-center overflow-hidden rounded-t-xl">
+            <div className="relative h-44 w-80 md:h-52 md:w-96">
               <Image
                 src={project.src}
                 alt="Logo"
                 fill
                 style={{ objectFit: "fill" }}
-                sizes="2.5rem"
+                sizes="24rem"
               />
             </div>
           </div>
-          <div>
-            <div className="font-semibold">{project.title}</div>
-            <div className="font-light">{project.desc}</div>
+          <div className="flex flex-col gap-1 p-4">
+            <div className="text-base font-semibold md:text-lg">
+              {project.title}
+            </div>
+            <div className="text-sm font-light md:text-base">
+              {project.desc}
+            </div>
           </div>
         </a>
       ))}
@@ -32,10 +36,10 @@ export default function Projects() {
 
 const projects = [
   {
-    title: "Coming soon.",
-    desc: "This is the project description",
-    href: "https://github.com/zhamann",
-    src: "/icons/nextjs.svg",
+    title: "TDEE Calculator",
+    desc: "This Next.js project provides a simple way to calculate basal metabolic rate (BMR) and total daily energy expenditure (TDEE).",
+    href: "https://tdee-calc.vercel.app",
+    src: "/projects/tdee.png",
   },
   {
     title: "Coming soon.",
--- src/app/_components/ProjectsPage.tsx
+++ src/app/_components/ProjectsPage.tsx
@@ -2,8 +2,8 @@ import Projects from "./Projects";
 
 export default function ProjectsPage() {
   return (
-    <main className="relative flex h-svh flex-col justify-center gap-8 bg-slate-950 px-4 py-8 text-white md:gap-12 md:px-16 md:py-0">
-      <div className="text-center md:text-start">
+    <main className="relative flex h-svh flex-col justify-center gap-4 bg-slate-950 py-8 text-white md:py-0">
+      <div className="px-4 text-center md:px-16 md:text-start">
         <div className="text-6xl font-semibold">
           My <span className="text-indigo-500">Projects</span>
         </div>
