9338d30a5d39cda275f338e4b53c5ab2b58f8cc5
Zac Hamann <hamannzac@gmail.com>
2024-10-05 09:18:28 -0500

    calculate focus on scroll

--- src/app/_components/Projects.tsx
+++ src/app/_components/Projects.tsx
@@ -1,13 +1,68 @@
+import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 
+const mdBreakpoint = 768;
+
 export default function Projects() {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (containerRef.current) {
+        const container = containerRef.current;
+        const isBelowMd = window.innerWidth < mdBreakpoint;
+
+        // use containerCenter if below 'md', otherwise use containerStart
+        const containerPosition = isBelowMd
+          ? container.scrollLeft + container.offsetWidth / 2
+          : container.scrollLeft;
+
+        const closestCard = Array.from(container.children).reduce(
+          (closest, card, index) => {
+            const cardElement = card as HTMLElement;
+            const cardCenter = cardElement.offsetLeft;
+            const distance = Math.abs(containerPosition - cardCenter);
+
+            if (distance < closest.distance) {
+              return { index, distance };
+            }
+
+            return closest;
+          },
+          { index: 0, distance: Infinity },
+        );
+
+        setActiveIndex(closestCard.index);
+      }
+    };
+
+    const container = containerRef.current;
+    if (container) {
+      container.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      if (container) {
+        container.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, []);
+
   return (
-    <div className="flex shrink-0 items-stretch gap-4 overflow-x-auto py-6 pe-8 ps-[50vw] md:gap-8 md:pe-16 md:ps-16">
+    <div
+      ref={containerRef}
+      className="flex shrink-0 items-stretch gap-4 overflow-x-auto py-6 pe-8 ps-[50vw] md:gap-8 md:pe-16 md:ps-16"
+    >
       {projects.map((project, index) => (
         <a
           key={index}
           href={project.href}
-          className="flex -translate-x-1/2 flex-col rounded-lg border border-slate-800 bg-slate-900 transition-transform duration-300 ease-in-out hover:z-10 hover:scale-105 hover:border-indigo-500 md:translate-x-0"
+          className={`flex -translate-x-1/2 flex-col rounded-lg border transition-transform duration-300 ease-in-out md:translate-x-0 ${
+            index === activeIndex
+              ? "z-10 scale-105 border-indigo-500"
+              : "border-slate-800"
+          }`}
         >
           <div className="flex items-center justify-center overflow-hidden rounded-t-xl">
             <div className="relative h-44 w-80 md:h-52 md:w-96">
